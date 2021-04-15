b0e8f2b02356aee8054ed5ce339c7a06da558903
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-15 15:30:57 -0300

    corrigindo formatacao lint

--- script.js
+++ script.js
@@ -27,50 +27,30 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-async function fetchMLB (shopping) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${shopping}`)
-  const products = await response.json()
+async function fetchMLB(shopping) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${shopping}`);
+  const products = await response.json();
   return products;
-};
+}
 
-function renderProducts (products) {
+function renderProducts(products) {
   products.results.forEach((product) => {
     const allProducts = {
           sku: product.id,
           name: product.title,
           image: product.thumbnail,
-        }
+        };
     document.querySelector('.items')
-      .appendChild(createProductItemElement(allProducts))
-  })
+      .appendChild(createProductItemElement(allProducts));
+  });
 }
-
 
 // retorna o item pelo id passado pelo elemento span.item__sku
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText; // retorna o text que tem dentro da class item__sku que é filho de Item
 }
 
-async function fetchItemsById (event) {
-  const ItemID = getSkuFromProductItem(event.target.parentNode) // parentNode é a class Item que será passada para a função getSku...
-  const responseItem = await fetch(`https://api.mercadolibre.com/items/${ItemID}`)
-  const products = await responseItem.json() // captura os items
-  const productsCard = {
-    sku: products.id,
-    name: products.title,
-    salePrice: products.price,
-  }
-  // capturar a lista onde vao ser add os produtos através da função createCartItemElement
-  document.querySelector('.cart__items')
-    .appendChild(createCartItemElement(productsCard))
-}
-
-function getBtn () {
-  document.querySelectorAll('.item button')
-    .forEach((buttonAdd) => buttonAdd.addEventListener('click', fetchItemsById))
-}
-
-function cartItemClickListener(event) {
+function cartItemClickListener(_event) {
   // coloque seu código aqui
 }
 
@@ -81,6 +61,25 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function fetchItemsById(event) {
+  const ItemID = getSkuFromProductItem(event.target.parentNode); // parentNode é a class Item que será passada para a função getSku...
+  const responseItem = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
+  const products = await responseItem.json(); // captura os items
+  const productsCard = {
+    sku: products.id,
+    name: products.title,
+    salePrice: products.price,
+  };
+  // capturar a lista onde vao ser add os produtos através da função createCartItemElement
+  document.querySelector('.cart__items')
+    .appendChild(createCartItemElement(productsCard));
+}
+
+function getBtn() {
+  document.querySelectorAll('.item button')
+    .forEach((buttonAdd) => buttonAdd.addEventListener('click', fetchItemsById));
 }
 
 window.onload = async function onload() { 
