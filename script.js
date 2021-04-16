59ecf5d726c94843946240443b889794d3fd471b
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-16 19:11:19 -0300

    adiciona e remove os items ao localStorage

--- script.js
+++ script.js
@@ -23,36 +23,50 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
 
+// captura os produtos da api
 async function fetchMLB(shopping) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${shopping}`);
   const products = await response.json();
-  return products;
+  return products.results;
 }
 
+// renderiza os produtos na tela
 function renderProducts(products) {
-  products.results.forEach((product) => {
+  const elementProducts = document.querySelector('.items');
+  products.forEach((product) => {
     const allProducts = {
           sku: product.id,
           name: product.title,
           image: product.thumbnail,
         };
-    document.querySelector('.items')
-      .appendChild(createProductItemElement(allProducts));
+      const creatProducts = createProductItemElement(allProducts);
+      elementProducts.appendChild(creatProducts);
   });
 }
 
-// retorna o item pelo id passado pelo elemento span.item__sku
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText; // retorna o text que tem dentro da class item__sku que é filho de Item
+const cartItems = '.cart__items';
+
+// resolução do requisito 4 feito com ajuda do Dennis
+function saveLocalStorage() {
+  const getCartList = document.querySelector(cartItems).innerHTML;
+  localStorage.setItem('card', getCartList);
 }
 
+// remove os items clicando neles no carrinho de compras e no local storage
 function cartItemClickListener(event) {
   // coloque seu código aqui
-  event.target.remove();
+  const getItem = event.target;
+  getItem.remove();
+  saveLocalStorage();
+}
+
+function loadLocalStorage() {
+  const getCartList = document.querySelector(cartItems);
+  getCartList.innerHTML = localStorage.getItem('card');
+  getCartList.addEventListener('click', cartItemClickListener);
 }
 
 // vai receber um objeto com sku, name e saleprice e faz a destrutucring para retornar um texto com id, name...
@@ -64,7 +78,12 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function fetchItemsById(event) {
+// retorna o item pelo id passado pelo elemento span.item__sku
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText; // retorna o text que tem dentro da class item__sku que é filho de Item
+}
+
+async function addItemCard(event) {
   const ItemID = getSkuFromProductItem(event.target.parentNode); // parentNode é a class Item que será passada para a função getSku...
   const responseItem = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
   const products = await responseItem.json(); // captura os items
@@ -73,17 +92,22 @@ async function fetchItemsById(event) {
     name: products.title,
     salePrice: products.price,
   };
-  // capturar a lista onde vao ser add os produtos através da função createCartItemElement
-  document.querySelector('.cart__items')
-    .appendChild(createCartItemElement(productsCard));
+  // retorna a lista onde vai ser add os produtos através da função createCartItemElement
+  const createCart = createCartItemElement(productsCard);
+  const getCartItems = document.querySelector(cartItems);
+  getCartItems.appendChild(createCart);
+  saveLocalStorage();
 }
 
 function getBtn() {
-  document.querySelectorAll('.item button')
-    .forEach((buttonAdd) => buttonAdd.addEventListener('click', fetchItemsById));
+  const getButton = document.querySelectorAll('.item button');
+  getButton.forEach((buttonAdd) => {
+      buttonAdd.addEventListener('click', addItemCard);
+    });
 }
 
 window.onload = async function onload() { 
+  loadLocalStorage();
   const products = await fetchMLB('computador');
   renderProducts(products);
   getBtn();
